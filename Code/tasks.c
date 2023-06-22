367eb5e83be87592cf4a94960b586a830b5f8389
Georg Bauer <bauer@rohmann.de>
2023-06-22 12:47:10 +0200

    print debug infos

--- Code/tasks.c
+++ Code/tasks.c
@@ -20,6 +20,7 @@
 #include "rtc.h"
 #include "displayMatrix.h"
 #include "ledMatrix.h"
+#include "usart0.h"
 
 //! Extern global variables
 extern volatile struct systemParameter systemConfig;
@@ -68,6 +69,51 @@ void taskSecond(void)
 {
 	// toggle status led
 	toggleStatusGreen();
+	
+	usart0ReceiveTransmit(0x0d); // CR
+	usart0ReceiveTransmit(0x0a); // LF
+	
+	usart0ReceiveTransmit(0x43); // C
+	usart0ReceiveTransmit(0x75); // u
+	usart0ReceiveTransmit(0x72); // r
+	usart0ReceiveTransmit(0x72); // r
+	usart0ReceiveTransmit(0x65); // e
+	usart0ReceiveTransmit(0x6e); // n
+	usart0ReceiveTransmit(0x74); // t
+	usart0ReceiveTransmit(0x20); // (space)
+	usart0ReceiveTransmit(0x54); // T
+	usart0ReceiveTransmit(0x69); // i
+	usart0ReceiveTransmit(0x6d); // m
+	usart0ReceiveTransmit(0x65); // e
+	usart0ReceiveTransmit(0x3a); // :
+	usart0ReceiveTransmit(0x20); // (space)
+	usart0ReceiveTransmit(systemTime.hour);
+	usart0ReceiveTransmit(systemTime.minute);
+	usart0ReceiveTransmit(systemTime.second);
+	usart0ReceiveTransmit(0x0d); // CR
+	usart0ReceiveTransmit(0x0a); // LF
+
+	usart0ReceiveTransmit(0x43); // C
+	usart0ReceiveTransmit(0x75); // u
+	usart0ReceiveTransmit(0x72); // r
+	usart0ReceiveTransmit(0x72); // r
+	usart0ReceiveTransmit(0x65); // e
+	usart0ReceiveTransmit(0x6e); // n
+	usart0ReceiveTransmit(0x74); // t
+	usart0ReceiveTransmit(0x20); // (space)
+	usart0ReceiveTransmit(0x44); // D
+	usart0ReceiveTransmit(0x61); // a
+	usart0ReceiveTransmit(0x74); // t
+	usart0ReceiveTransmit(0x65); // e
+	usart0ReceiveTransmit(0x3a); // :
+	usart0ReceiveTransmit(0x20); // (space)
+	usart0ReceiveTransmit(systemTime.year);
+	usart0ReceiveTransmit(systemTime.month);
+	usart0ReceiveTransmit(systemTime.day);
+	usart0ReceiveTransmit(systemTime.weekday);
+	usart0ReceiveTransmit(0x0d); // CR
+	usart0ReceiveTransmit(0x0a); // LF
+	
 }
 
 //! Task half second
